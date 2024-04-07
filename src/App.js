f11ff1b97201800061797ed1030d378a575bdbd5
EvanW2k <125333331+EvanW2k@users.noreply.github.com>
2024-04-07 15:04:47 -0700

    Added profile
    A profile page can now be loaded, right now it has hard coded user info. 
    Also changed the routing to profile for different userIDs 
    Added a quiz page that is blank, and added routing similar to profile

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {React, useState, Fragment} from 'react';
+import Main from './Components/main/main';
+
+function App() {
+
+    return (
+      <div>
+        <Main />
+      </div>
+    );
+}
+
+export default App;
