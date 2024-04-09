72c3f9f829abcaa0e94674109249732c00c098d6
Zeroxa <2991971hz@gmail.com>
2024-04-08 20:52:23 -0700

    Add files via upload

--- quizsocial-api/app/Controllers/QuizController.js
+++ quizsocial-api/app/Controllers/QuizController.js
@@ -5,6 +5,35 @@ function now() {
     return dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 }
 
+const allQuizzes = async (ctx) => {
+    console.log('all quizzes called.');
+    return new Promise((resolve, reject) => {
+        const query = `
+                       SELECT *
+                        FROM 
+                            quizzes
+                        ORDER BY quizID
+                        `;
+        dbConnection.query({
+            sql: query,
+        }, (error, tuples) => {
+            if (error) {
+                console.log("Connection error in RoutesController::allRoutes", error);
+                return reject(error);
+            }
+            ctx.body = tuples;
+            ctx.status = 200;
+            return resolve();
+        });
+    }).catch(err => {
+        console.log("Database connection error in allRoutes.", err);
+        // The UI side will have to look for the value of status and
+        // if it is not 200, act appropriately.
+        ctx.body = [];
+        ctx.status = 500;
+    });
+}
+
 const getQuizById = (ctx) => {
     return new Promise((resolve, reject) => {
         const query = `
@@ -34,10 +63,7 @@ const getQuizById = (ctx) => {
 
 const getQuestionsForQuiz = (ctx) => {
     return new Promise((resolve, reject) => {
-        const query = `
-            SELECT * FROM questions
-            WHERE quizID = ?
-        `;
+        const query = "SELECT * FROM questions WHERE quizID = ?";
         dbConnection.query({
             sql: query,
             values: [ctx.params.quizID]
@@ -59,7 +85,9 @@ const getQuestionsForQuiz = (ctx) => {
     });
 }
 
+
 module.exports = {
     getQuizById,
-    getQuestionsForQuiz
+    getQuestionsForQuiz,
+    allQuizzes
 };
