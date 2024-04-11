0c6527fa56f10de988fe5e892dca121009289b49
Zeroxa <2991971hz@gmail.com>
2024-04-10 23:44:34 -0700

    Add files via upload

---
+++ quizsocial-ui/src/API_Interface/API_Interface.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const AxiosConfigured = () => {
+    // Indicate to the API that all requests for this app are AJAX
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+    // Set the baseURL for all requests to the API domain instead of the current domain
+    // axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+    axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+
+
+    // Allow the browser to send cookies to the API domain (which include auth_token)
+    axios.defaults.withCredentials = true;
+
+
+//    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token;
+
+    return axios;
+};
+
+
+const axiosAgent = AxiosConfigured();
+
+export default class APIInterface {
+
+    async getUserInfo(userID) {
+        return axiosAgent.get(`login/${userID}`)
+            .then(userInfo => userInfo.data)
+            .catch(error => (
+                {
+                    error,
+                    user: undefined
+                 }));
+    }
+
+    async allQuizzes() {
+        return axiosAgent.get(`routes/all-quizzes`);
+    }
+
+
+    async getQuestionsForQuiz(quizID){
+        return axiosAgent.get(`routes/${quizID}/questions`);
+    }
+
+    async getQuizById(quizID) {
+        return axiosAgent.get(`routes/${quizID}`);
+    }
+
+
+}
---
+++ quizsocial-ui/src/App.js
@@ -0,0 +1,14 @@
+import React,{useState, Fragment} from 'react';
+import Main from './Main';
+
+
+function App() {
+
+    return (
+        <div>
+            <Main />
+        </div>
+    );
+}
+
+export default App;
