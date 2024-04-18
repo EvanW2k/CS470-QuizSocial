365fd369e8f646e26eab3345d65465f3ea5fdf70
Zeroxa <2991971hz@gmail.com>
2024-04-18 16:34:16 -0700

    Add files via upload

--- database/quizsocial.sql
+++ database/quizsocial.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.3.0, for Win64 (x86_64)
+-- MariaDB dump 10.19-11.3.2-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: quizsocial
 -- ------------------------------------------------------
--- Server version	8.3.0
+-- Server version	11.3.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,18 +21,18 @@
 
 DROP TABLE IF EXISTS `questions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `questions` (
-  `questionID` int NOT NULL AUTO_INCREMENT,
-  `quizID` int DEFAULT NULL,
-  `question` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `answer` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `questionID` int(11) NOT NULL AUTO_INCREMENT,
+  `quizID` int(11) DEFAULT NULL,
+  `question` text NOT NULL,
+  `answer` text NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`questionID`),
   KEY `quiz_set_id` (`quizID`),
   CONSTRAINT `fk_quiz_id_2` FOREIGN KEY (`quizID`) REFERENCES `quizzes` (`quizID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,24 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-INSERT INTO `questions` VALUES (1,1,'question1','answer1','2024-04-08 01:25:26','2024-04-08 01:25:26'),(2,1,'question2','answer2','2024-04-08 01:25:49','2024-04-08 01:25:49'),(3,2,'1+1','2','2024-04-13 17:38:00','2024-04-13 17:38:00'),(4,2,'7-3','4','2024-04-13 17:38:00','2024-04-13 17:38:00'),(5,2,'100+0','100','2024-04-13 17:38:00','2024-04-13 17:38:00'),(6,2,'2+8','10','2024-04-13 17:38:00','2024-04-13 17:38:00'),(7,3,'1x1','1','2024-04-13 17:39:08','2024-04-13 17:39:08'),(8,3,'10x10','100','2024-04-13 17:39:08','2024-04-13 17:39:08'),(9,3,'8/2','4','2024-04-13 17:39:08','2024-04-13 17:39:08'),(10,3,'3x11','33','2024-04-13 17:39:08','2024-04-13 17:39:08');
+INSERT INTO `questions` VALUES
+(1,1,'question1','answer1','2024-04-08 01:25:26','2024-04-08 01:25:26'),
+(2,1,'question2','answer2','2024-04-08 01:25:49','2024-04-08 01:25:49'),
+(3,2,'1+1','2','2024-04-13 17:38:00','2024-04-13 17:38:00'),
+(4,2,'7-3','4','2024-04-13 17:38:00','2024-04-13 17:38:00'),
+(5,2,'100+0','100','2024-04-13 17:38:00','2024-04-13 17:38:00'),
+(6,2,'2+8','10','2024-04-13 17:38:00','2024-04-13 17:38:00'),
+(7,3,'1x1','1','2024-04-13 17:39:08','2024-04-13 17:39:08'),
+(8,3,'10x10','100','2024-04-13 17:39:08','2024-04-13 17:39:08'),
+(9,3,'8/2','4','2024-04-13 17:39:08','2024-04-13 17:39:08'),
+(10,3,'3x11','33','2024-04-13 17:39:08','2024-04-13 17:39:08'),
+(11,1,'What is the capital city of Japan?','Tokyo','2024-04-17 10:27:33','2024-04-17 10:27:33'),
+(12,1,'Which planet is known as the Red Planet?','Mars','2024-04-17 10:27:33','2024-04-17 10:27:33'),
+(13,1,'What year did the Berlin Wall fall?','1989','2024-04-17 10:32:07','2024-04-17 10:32:07'),
+(14,1,'Who discovered penicillin?','Alexander Fleming','2024-04-17 10:32:07','2024-04-17 10:32:07'),
+(15,1,'What is the hardest natural substance on Earth?','Diamond','2024-04-17 10:32:07','2024-04-17 10:32:07'),
+(16,1,'What is the largest country in the world?','Russia','2024-04-17 10:32:07','2024-04-17 10:32:07'),
+(17,1,'What novel is often cited as the bestselling single-volume book of all time?','Don Quixote','2024-04-17 10:32:07','2024-04-17 10:32:07');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,11 +68,11 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `quiz_favorites`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `quiz_favorites` (
-  `quizID` int DEFAULT NULL,
-  `userID` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `favorited_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `quizID` int(11) DEFAULT NULL,
+  `userID` char(36) NOT NULL,
+  `favorited_date` timestamp NULL DEFAULT current_timestamp(),
   KEY `fk_user_id` (`userID`),
   KEY `fk_quiz_id` (`quizID`),
   CONSTRAINT `fk_quiz_id` FOREIGN KEY (`quizID`) REFERENCES `quizzes` (`quizID`),
@@ -69,7 +86,10 @@ CREATE TABLE `quiz_favorites` (
 
 LOCK TABLES `quiz_favorites` WRITE;
 /*!40000 ALTER TABLE `quiz_favorites` DISABLE KEYS */;
-INSERT INTO `quiz_favorites` VALUES (2,'k.yuen','2024-04-16 02:08:47'),(2,'h.zhang','2024-04-16 02:10:44'),(3,'h.zhang','2024-04-16 02:10:51');
+INSERT INTO `quiz_favorites` VALUES
+(2,'k.yuen','2024-04-16 02:08:47'),
+(2,'h.zhang','2024-04-16 02:10:44'),
+(3,'h.zhang','2024-04-16 02:10:51');
 /*!40000 ALTER TABLE `quiz_favorites` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -109,15 +129,15 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `quizzes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `quizzes` (
-  `quizID` int NOT NULL AUTO_INCREMENT,
-  `userID` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `num_favorites` int NOT NULL DEFAULT '0',
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `quizID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` char(36) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `num_favorites` int(11) NOT NULL DEFAULT 0,
+  `description` text DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`quizID`),
   KEY `user_id` (`userID`),
   CONSTRAINT `fk_userID` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
@@ -131,7 +151,10 @@ CREATE TABLE `quizzes` (
 
 LOCK TABLES `quizzes` WRITE;
 /*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
-INSERT INTO `quizzes` VALUES (1,'b23c2d39-f009-11ee-b93a-085bd6555b53','Test set',0,'Quiz set for testing','2024-04-08 01:18:43','2024-04-08 01:18:43'),(2,'e.walters','My Math quiz #1',2,NULL,'2024-04-13 17:35:36','2024-04-15 19:10:44'),(3,'e.walters','My Math quiz #2',1,NULL,'2024-04-13 17:35:40','2024-04-15 19:10:51');
+INSERT INTO `quizzes` VALUES
+(1,'b23c2d39-f009-11ee-b93a-085bd6555b53','Test set',0,'Quiz set for testing','2024-04-08 01:18:43','2024-04-08 01:18:43'),
+(2,'e.walters','My Math quiz #1',2,NULL,'2024-04-13 17:35:36','2024-04-15 19:10:44'),
+(3,'e.walters','My Math quiz #2',1,NULL,'2024-04-13 17:35:40','2024-04-15 19:10:51');
 /*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,11 +164,11 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user_follows`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_follows` (
-  `follower_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `followed_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `followed_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `follower_id` char(36) NOT NULL,
+  `followed_id` char(36) NOT NULL,
+  `followed_date` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`follower_id`,`followed_id`),
   KEY `followed_id` (`followed_id`),
   CONSTRAINT `user_follows_ibfk_1` FOREIGN KEY (`follower_id`) REFERENCES `users` (`userID`),
@@ -159,9 +182,48 @@ CREATE TABLE `user_follows` (
 
 LOCK TABLES `user_follows` WRITE;
 /*!40000 ALTER TABLE `user_follows` DISABLE KEYS */;
-INSERT INTO `user_follows` VALUES ('e.walters','h.zhang','2024-04-13 17:05:56'),('e.walters','k.yuen','2024-04-13 17:05:49'),('h.zhang','e.walters','2024-04-13 17:06:34'),('h.zhang','k.yuen','2024-04-13 17:06:45'),('k.yuen','e.walters','2024-04-13 17:06:16'),('k.yuen','h.zhang','2024-04-13 17:06:05');
+INSERT INTO `user_follows` VALUES
+('e.walters','h.zhang','2024-04-13 17:05:56'),
+('e.walters','k.yuen','2024-04-13 17:05:49'),
+('h.zhang','e.walters','2024-04-17 17:06:45'),
+('h.zhang','k.yuen','2024-04-13 17:06:45'),
+('k.yuen','h.zhang','2024-04-13 17:06:05');
 /*!40000 ALTER TABLE `user_follows` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger follow_insert after insert on user_follows
+for each row
+update users set num_follows = num_follows + 1 where userID = new.followed_id */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger follow_delete after delete on user_follows
+for each row
+update users set num_follows = num_follows - 1 where userID = old.followed_id */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `user_profile`
@@ -169,10 +231,10 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user_profile`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_profile` (
-  `userID` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `bio` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `userID` char(36) NOT NULL,
+  `bio` text DEFAULT NULL,
   PRIMARY KEY (`userID`),
   CONSTRAINT `user_profile_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -184,7 +246,11 @@ CREATE TABLE `user_profile` (
 
 LOCK TABLES `user_profile` WRITE;
 /*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
-INSERT INTO `user_profile` VALUES ('b23c2d39-f009-11ee-b93a-085bd6555b53',''),('e.walters','Hello my name is Evan. I like making quizzes about math, cs, and sometimes geography!'),('h.zhang','My name is Hanpei, This is my quiz social page!'),('k.yuen','Helllllo');
+INSERT INTO `user_profile` VALUES
+('b23c2d39-f009-11ee-b93a-085bd6555b53',''),
+('e.walters','Hello my name is Evan. I like making quizzes about math, cs, and sometimes geography!'),
+('h.zhang','My name is Hanpei, This is my quiz social page!'),
+('k.yuen','Helllllo');
 /*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,14 +260,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `userID` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `userID` char(36) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `num_follows` int(11) DEFAULT 0,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
@@ -214,7 +281,11 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('b23c2d39-f009-11ee-b93a-085bd6555b53','newUser','newuser@example.com','userPassword','2024-04-01 02:24:16','2024-04-01 02:24:16'),('e.walters','Evan 2k','waltersev@sonoma.edu','password','2024-04-13 17:02:21','2024-04-13 17:02:21'),('h.zhang','Zeroxa','zhangha@sonoma.edu','password','2024-04-13 17:04:13','2024-04-13 17:04:13'),('k.yuen','Kathy','yuenk@sonoma.edu','password','2024-04-13 17:03:48','2024-04-13 17:03:48');
+INSERT INTO `users` VALUES
+('b23c2d39-f009-11ee-b93a-085bd6555b53','newUser','newuser@example.com','userPassword','2024-04-01 02:24:16','2024-04-01 02:24:16',0),
+('e.walters','Evan 2k','waltersev@sonoma.edu','password','2024-04-13 17:02:21','2024-04-17 17:06:45',1),
+('h.zhang','Zeroxa','zhangha@sonoma.edu','password','2024-04-13 17:04:13','2024-04-17 16:56:44',2),
+('k.yuen','Kathy','yuenk@sonoma.edu','password','2024-04-13 17:03:48','2024-04-17 16:56:44',2);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -243,4 +314,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-15 19:12:34
+-- Dump completed on 2024-04-18 16:33:19
